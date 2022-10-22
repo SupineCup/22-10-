c3439be9af6648722e400271651373627319795c
SupineCup <114007597+SupineCup@users.noreply.github.com>
2022-10-22 19:36:34 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std; 
+int main() {
+	//Series1◦.Даны десять вещественных чисел.Найти их сумму.
+	/*double x, sum = 0;
+	int i = 1;
+	while (i <= 10) {
+	cin >> x;
+	sum += x;
+	i++;
+	}
+	cout << sum;*/
+
+	//Series2.Даны десять вещественных чисел.Найти их произведение.
+	double x, pro = 1;
+	int i = 1;
+	while (i <= 10) {
+		for (i = 1; i <= 10; ++i) {
+			cin >> x;
+			pro += x;
+			i = i * x;
+		}
+		cout << pro;
+	}
+	
+	//Series3.Даны десять вещественных чисел.Найти их среднее арифметическое.
+	/*double x, arefm = 1;
+	int i = 1;
+	arefm = x;
+	while (i <= 10) {
+		for (i = 1; i >= 10 )
+	}*/	
+
+
+}
